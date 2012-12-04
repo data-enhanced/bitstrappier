3f7f30fd50cb58e8da209cfa7aacb71417160a60
David Cochran <dc@cochran-air.att.net>
2012-12-04 14:33:47 -0600

    Dropdown Menu JS fix for Touch - BS 2.1 - 2.2.1

--- js/main-ck.js
+++
@@ -1,3 +0,0 @@
-/* Author: David Cochran
-
-*/$(document).ready(function(){$("body").on("touchstart.dropdown",".dropdown-menu",function(e){e.stopPropagation()})});
--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@
 
 $(document).ready(function() {
 
-		// Fix Twitter Bootstrap Dropdown menu in iOS 5+ and Android
+		// Fix Twitter Bootstrap 2.1 - 2.2.1 dropdown menu in iOS 5+ and Android
 		// https://github.com/twitter/bootstrap/issues/4550#issuecomment-8525357
 			$('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
+			// an alternative possible fix
+			// https://github.com/twitter/bootstrap/issues/4550#issuecomment-10986399
 
 });
 
